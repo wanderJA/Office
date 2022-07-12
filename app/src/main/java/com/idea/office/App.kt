7991c5fb109cde89d5e67697729be05935bad796
brestwang <brestwang@tencent.com>
2022-07-13 00:14:59 +0800

    Initial commit

---
+++ app/src/main/java/com/idea/office/App.kt
@@ -0,0 +1,16 @@
+package com.idea.office
+
+import android.app.Application
+
+/**
+ * Copyright (c) 2022 Tencent. All rights reserved.
+ * 类功能描述:
+ *
+ * @author brestwang
+ * @date 2022/7/10
+ */
+class App: Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
